458da967092adc193e371e87ccd2455d2ef9e253
aadiraj1 <aadiraj341@gmail.com>
2025-05-03 18:25:08 +0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: './Sparkpro/',  
+  base: '/Sparkpro/',  
   plugins: [react()],
   resolve: {
     alias: {
